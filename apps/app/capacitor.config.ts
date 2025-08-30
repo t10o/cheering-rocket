9af29f1e7cf0d34d7c8072ed0227adc0935a6441
t10o <dev@t10o.one>
2025-08-30 17:59:24 +0900

    雛形作成

---
+++ apps/app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cheering.rocket',
+  appName: 'CheeringRocket',
+  webDir: 'dist'
+};
+
+export default config;
