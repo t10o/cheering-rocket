dda45b1664c1a7e200419f5ff477847933f0af81
t10o <dev@t10o.one>
2025-09-27 23:55:03 +0900

    アプリ名変更

--- apps/app/capacitor.config.ts
+++ apps/app/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.cheering.rocket",
-  appName: "CheeringRocket",
+  appName: "応援ロケット",
   webDir: "dist",
   server: {
     androidScheme: "https",
